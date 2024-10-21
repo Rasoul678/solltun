b940c6e62f54ef194834095898fe4aa1637993a4
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-10-21 17:46:37 +0330

    :construction: WIP: style :)

--- src/functions_lib.rs
+++ src/functions_lib.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 
 use crate::{AppConfig, AppLogic, AppWindow, Todo};
 use ohmydb::JsonDB;
-use slint::{ComponentHandle, Timer, TimerMode};
+use slint::ComponentHandle;
 
 pub fn on_init_db(ui: &AppWindow) {
     let logic = ui.global::<AppLogic>();
@@ -12,24 +12,13 @@ pub fn on_init_db(ui: &AppWindow) {
         let ui = ui_handle.upgrade().unwrap();
         let ui_handle = ui.as_weak();
 
-        let timer = Timer::default();
-        timer.start(
-            TimerMode::SingleShot,
-            std::time::Duration::from_millis(1000),
-            move || {
-                println!("Rocket will be launched after 1000ms.");
-            },
-        );
-
         let cfg = ui.global::<AppConfig>();
         let db_name = cfg.get_db_name();
 
         tokio::runtime::Handle::current().spawn(async move {
-            // TODO: fix empty content bug
-            let db: JsonDB<Todo> = JsonDB::new(&db_name).await.unwrap();
+            let mut db: JsonDB<Todo> = JsonDB::new(&db_name).await.unwrap();
             let db_path = db.get_db_path().to_string();
-            // db.add_table("users").await.unwrap();
-            // db.add_table("products").await.unwrap();
+            db.add_table("today").await.unwrap();
             let tables = db.get_db_tables().await;
 
             ui_handle.upgrade_in_event_loop(move |ui| {
@@ -43,8 +32,9 @@ pub fn on_init_db(ui: &AppWindow) {
                 let cfg = ui.global::<AppConfig>();
                 cfg.set_db_path(db_path.into());
                 cfg.set_tables(tables_model.into());
-                cfg.set_is_rocket_launched(!cfg.get_is_rocket_launched());
             })
         });
     });
+
+    logic.invoke_init_db();
 }
